2b7f4b4a93b819b6b71fff79491167540144b37b
DP@arturo <redstonefreedom@gmail.com>
2018-08-01 19:06:57 +0000

    numbers

--- main.py
+++ main.py
@@ -8,34 +8,66 @@ class Token(object):
 
 # foo(+3)
 class Plus(Token):
-    pass
+    def __repr__(self):
+        return 'Plus()'
 
 class Number(Token):
+    def __repr__(self):
+        return 'Number(%d)' % (self.n,)
     def __init__(self, n):
+        if not isinstance(n, int):
+            raise ValueError('Number must be initialised with a number')
         self.n = n
 
 class SemiColon(Token):
+    def __repr__(self):
+        return 'SemiColon()'
+
+class TokenizerError(Exception):
     pass
 
+def isNumberChar(b):
+    return b in b'0123456789'
+
+def peek1(buf):
+    return buf.peek(1)[:1]
+
 def tokenize(buf):
-    peek = buf.peek(1) # see what's up next
-    if peek == ';':
-        buf.read1(1) # consume the semicolon
-        return SemiColon()
-    if peek == '+':
-        buf.read1(1) # consume the +
-        return Plus()
-    if peek in range(0,10):
-        buf.read1(1) # consume the +
-        return Number(peek)
-    if peek == ' ':
-        buf.read1(1)
-        # just consume and move on
+    while True:
+        peek = peek1(buf) # see what's up next
+        if peek == b'':
+            break
+        if peek == b';':
+            buf.read1(1) # consume the semicolon
+            yield SemiColon()
+            continue
+        if peek == b'+':
+            buf.read1(1) # consume the +
+            yield Plus()
+            continue
+        if isNumberChar(peek):
+            l = []
+            while isNumberChar(peek):
+                l.append(buf.read1(1))
+                peek = peek1(buf)
+            nl = tuple(map(int, l))
+            n = map(lambda x: x[1] * 10**x[0], enumerate(reversed(nl)))
+            yield Number(sum(n))
+            continue
+        if peek == b' ':
+            buf.read1(1)
+            continue
+            # just consume and move on
+        if peek == b'\n':
+            buf.read1(1)
+            continue
+            # just consume and move on
+        raise TokenizerError("Unexpected character: %s" % (peek,))
 
 def main(argv):
     f = open(argv[1], 'rb') if len(argv) > 1 else sys.stdin
     buf = BufferedReader(f)
-    pprint(tokenize(buf))
+    pprint(list(tokenize(buf)))
 
 if __name__ == "__main__":
     main(sys.argv)
